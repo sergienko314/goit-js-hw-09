b900774f598a1242234c4324229bfc7d84c55ecd
Serggiyenko Mykyta <sergyenko1995@gmail.com>
2022-08-18 21:54:56 +0300

    test

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,7 +1,6 @@
 import flatpickr from 'flatpickr';
 // Дополнительный импорт стилей
 import 'flatpickr/dist/flatpickr.min.css';
-// import Notiflix from 'notiflix';
 
 // // one by one
 // import { Notify } from 'notiflix/build/notiflix-notify-aio';
@@ -16,20 +15,38 @@ const dataDays = document.querySelector('[data-days]');
 const dataHours = document.querySelector('[data-hours]');
 const dataMinutes = document.querySelector('[data-minutes]');
 const dataSeconds = document.querySelector('[data-seconds]');
-
-// onInputTimer.addEventListener('input');
-// dataStart.addEventListener('submit', dataStartSubmit());
-
-// flatpickr(onInputTimer, options);
 const myInput = document.querySelector('#datetime-picker');
 
-const fp = flatpickr(myInput, {
+dataStart.addEventListener('click', () => startTime());
+dataStart.disabled = true;
+function startTime() {
+  const intervalID = setInterval(() => {
+    const inputTime = new Date(myInput.value).getTime();
+    const daltaTime = inputTime - Date.now();
+    const { days, hours, minutes, seconds } = convertMs(daltaTime);
+    dataDays.textContent = days;
+    dataHours.textContent = hours;
+    dataMinutes.textContent = minutes;
+    dataSeconds.textContent = seconds;
+    console.log(daltaTime);
+    if (daltaTime <= 1000) {
+      clearInterval(intervalID);
+    }
+  }, 1000);
+}
+
+flatpickr(myInput, {
   enableTime: true,
   time_24hr: true,
-  defaultDate: new Date(),
+  defaultDate: Date.now(),
   minuteIncrement: 1,
   onClose(selectedDates) {
-    console.log(selectedDates[0]);
+    if (selectedDates[0] <= new Date()) {
+      Notiflix.Notify.failure('Please choose a date in the future');
+      dataStart.disabled = true;
+    } else {
+      dataStart.disabled = false;
+    }
   },
 });
 
@@ -40,14 +57,18 @@ function convertMs(ms) {
   const hour = minute * 60;
   const day = hour * 24;
 
+  function pad(value) {
+    return String(value).padStart(2, '0');
+  }
+
   // Remaining days
-  const days = Math.floor(ms / day);
+  const days = pad(Math.floor(ms / day));
   // Remaining hours
-  const hours = Math.floor((ms % day) / hour);
+  const hours = pad(Math.floor((ms % day) / hour));
   // Remaining minutes
-  const minutes = Math.floor(((ms % day) % hour) / minute);
+  const minutes = pad(Math.floor(((ms % day) % hour) / minute));
   // Remaining seconds
-  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+  const seconds = pad(Math.floor((((ms % day) % hour) % minute) / second));
 
   return { days, hours, minutes, seconds };
 }
